fd315ff7b16d4ae0c0c59ae60b6576b0b4d16b75
kitsuneislife <81118374+kitsuneislife@users.noreply.github.com>
2024-05-28 21:07:00 +0000

    Minor profile changes

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -16,7 +16,19 @@ function Profile() {
 
   return (
     <div className="profile">
-      <ProfileCanvas className="profile_canvas" />
+      <div className="profile_box">
+        <ProfileCanvas className="profile_canvas" />
+        <div className="profile_box_minus">
+          <div className="profile_edit_button">
+            <i class="fa-solid fa-palette"></i>
+            <span>Personalizar</span>
+          </div>
+          <div className="profile_edit_button">
+            <i class="fa-solid fa-pencil"></i>
+            <span>Editar</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
